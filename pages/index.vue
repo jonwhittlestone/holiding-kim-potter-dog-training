fb7fd898531f06ac6ecf797030bfdf5b62158e24
Jon Whittlestone <dev@howapped.com>
2019-11-22 18:00:12 +0000

    send-email

--- pages/index.vue
+++ pages/index.vue
@@ -14,6 +14,55 @@
             <a href="">Good Manners</a>
         </div>
         <div class="contact">Contact <a href="caninetrainerkp@gmail.com">caninetrainerkp@gmail.com</a></div>
+        
+        <!-- contact form -->
+
+        <article v-for="msg in messages"
+          :key="msg.text"
+          class="message"
+          :class="msg.type === 'success' ? 'is-success' : 'is-danger'">
+          <div class="message-body">
+            {  }
+          </div>
+        </article>
+
+
+        <section class="contact-form">
+          <div class="field">
+            <label class="label">Name</label>
+            <div class="control">
+              <input v-model="contactName" class="input" type="text">
+            </div>
+          </div>
+
+          <div class="field">
+            <label class="label">Email</label>
+            <div class="control">
+              <input v-model="contactEmail" class="input" type="email">
+            </div>
+          </div>
+
+          <div class="field">
+            <label class="label">Your Message</label>
+            <div class="control">
+              <textarea v-model="contactMessage" class="textarea" />
+            </div>
+          </div>
+
+          <div class="field is-grouped">
+            <div class="control">
+              <button class="button is-link" @click="sendMessage">
+                Send Message
+              </button>
+            </div>
+            <div class="control">
+              <button class="button is-text" @click="cancelMessage">
+                Cancel
+              </button>
+            </div>
+          </div>
+        </section>
+
       </div>
       </div>
    </div> 
@@ -22,8 +71,42 @@
 <script>
 
 export default {
-  
-}
+      data () {
+        return {
+          messages: [],
+          contactName: '',
+          contactEmail: '',
+          contactMessage: ''
+        }
+      },
+      methods: {
+        sendMessage () {
+          this.messages = []
+          this.triggerSendMessageFunction()
+        },
+        cancelMessage () {},
+        resetForm () {
+          this.messages = []
+          this.contactName = ''
+          this.contactEmail = ''
+          this.contactMessage = ''
+        },
+        async triggerSendMessageFunction () {
+          try {
+            console.debug('here');
+            const response = await this.$axios.$post('/.netlify/functions/send-contact-email', {
+              contactName: this.contactName,
+              contactEmail: this.contactEmail,
+              message: this.contactMessage
+            })
+            this.resetForm()
+            this.messages.push({ type: 'success', text: response })
+          } catch (error) {
+            this.messages.push({ type: 'error', text: error.response.data })
+          }
+        }
+      }
+    }
 </script>
 
 <style>
